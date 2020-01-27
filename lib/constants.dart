f7e976ffefaeaa9bafff1178139502dd0744ea5e
GOODBAES1 <59509018+GOODBAES1@users.noreply.github.com>
2020-01-27 09:36:01 +0600

    BORDER RADIUS AT REG FORM

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,9 +3,12 @@ const textImputDecorations = InputDecoration(
 
       filled: true,
       enabledBorder: OutlineInputBorder(
+
+          borderRadius: BorderRadius.all(Radius.circular(30)),
           borderSide: BorderSide(color: Colors.blue)
       ),
       focusedBorder: OutlineInputBorder(
+
           borderSide: BorderSide(color: Colors.deepOrangeAccent),
       )
 );
